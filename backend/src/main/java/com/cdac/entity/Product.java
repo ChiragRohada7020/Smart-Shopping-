ae5a744eb24237f4b1d31ecdee35909a64ce68a6
sanskar singh <sanskarkvssit@gmail.com>
2025-08-05 20:54:40 +0530

    cart has been successfully updated

--- backend/src/main/java/com/cdac/entity/Product.java
+++ backend/src/main/java/com/cdac/entity/Product.java
@@ -4,10 +4,13 @@ import lombok.*;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Embeddable;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Embeddable
 public class Product implements Serializable {
 
     private String id; // product ID (Amazon/Flipkart)
