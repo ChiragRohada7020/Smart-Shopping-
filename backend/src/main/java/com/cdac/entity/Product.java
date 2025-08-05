30f5ac607441b6683a189874af10067c7ac21457
root <root@LAPTOP-1H9ORDBI.localdomain>
2025-08-05 10:55:01 +0000

    upadted 1.0

--- backend/src/main/java/com/cdac/entity/Product.java
+++ backend/src/main/java/com/cdac/entity/Product.java
@@ -1,12 +1,12 @@
 package com.cdac.entity;
 
+
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import jakarta.persistence.Id ;
 
 @Entity
 @Getter
